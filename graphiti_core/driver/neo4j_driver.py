006d272156c1e4b44fe17d9790c2d3cb70964184
Ziyi Cui <ziyi.cui@optillusion.games>
2025-12-03 14:18:43 -0800

    Add debug log in neo4j_driver

--- graphiti_core/driver/neo4j_driver.py
+++ graphiti_core/driver/neo4j_driver.py
@@ -55,6 +55,7 @@ class Neo4jDriver(GraphDriver):
 
         try:
             result = await self.client.execute_query(cypher_query_, parameters_=params, **kwargs)
+            print (f'[DEBUG] neo4j execute_query Result: {result}')
         except Exception as e:
             logger.error(f'Error executing Neo4j query: {e}\n{cypher_query_}\n{params}')
             raise
